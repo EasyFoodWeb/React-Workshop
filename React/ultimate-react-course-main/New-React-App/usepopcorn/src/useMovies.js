5faffdaa0ba134184136cf07e166f85707a4672a
UMER-YOUSAF/DT <umaryousaf93@gmail.com>
2024-05-07 13:07:31 +0500

    Commit 05072024

---
+++ React/ultimate-react-course-main/New-React-App/usepopcorn/src/useMovies.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+const KEY = "10413753";
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      callback?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setError("");
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movies Not Found");
+
+          setMovies(data.Search);
+          //console.log(data.Search);
+          setIsLoading(false);
+          setError("");
+        } catch (err) {
+          console.error(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+        //console.log(data.Search);
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      //handleCloseMovie();
+      fetchMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
