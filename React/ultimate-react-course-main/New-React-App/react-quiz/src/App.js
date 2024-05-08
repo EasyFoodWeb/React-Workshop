ec276571b49163d97de6f910800ea1aa1e309731
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-05-08 21:16:36 +0500

    Reacr Quiz last 08052024

--- React/ultimate-react-course-main/New-React-App/react-quiz/src/App.js
+++ React/ultimate-react-course-main/New-React-App/react-quiz/src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -21,13 +25,20 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
     default:
       throw new Error("Action UnKnown");
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -41,8 +52,12 @@ function App() {
         <Header />
 
         <Main>
-          <p>1/15</p>
-          <p>Question</p>
+          {status === "loading" && <Loader />}
+          {status === "error" && <Error />}
+          {status === "ready" && (
+            <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+          )}
+          {status === "active" && <Question />}
         </Main>
       </div>
     </div>
