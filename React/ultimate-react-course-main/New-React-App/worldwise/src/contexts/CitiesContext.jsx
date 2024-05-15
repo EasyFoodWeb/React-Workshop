b30f8adc2c69a3a88dd27ef7c20496612eb24e66
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-05-15 13:50:31 +0500

    World Wise - Context APIs

---
+++ React/ultimate-react-course-main/New-React-App/worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { useEffect, createContext, useState, useContext } from "react";
+
+const BASE_URL = "http://localhost:8000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error while fetching cities!!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("Error while fetching cities!!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const cities = useContext(CitiesContext);
+  if (cities === undefined)
+    throw new Error("Cities context was used outside the CitieProvider.");
+  return cities;
+}
+
+export { CitiesProvider, useCities };
