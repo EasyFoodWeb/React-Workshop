4645b9fac3f363432ebc1e3ec0db844a6ce6d04d
M Zain ul Abaidin <73293952+ThrowFF@users.noreply.github.com>
2024-11-10 22:35:21 +0500

    Delete booking

--- React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -11,8 +11,11 @@ import Spinner from "../../ui/Spinner";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
-import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { HiArrowUpOnSquare, HiTrash } from "react-icons/hi2";
 import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -23,6 +26,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
   const { checkout, isCheckingOut } = useCheckout();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
   const navigate = useNavigate();
   //const status = "checked-in";
 
@@ -65,6 +69,24 @@ function BookingDetail() {
             Check Out
           </Button>
         )}
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger" icon={<HiTrash />}>
+              Delete Booking
+            </Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
+            />
+          </Modal.Window>
+        </Modal>
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
--- React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -104,7 +104,7 @@ function CabinRow({ cabin }) {
 
             <Modal.Window name="edit">
               <CreateCabinForm cabinToEdit={cabin} />
-            </Modal.Window>
+            </Modal.Window> 
 
             <Modal.Window name="delete">
               <ConfirmDelete
