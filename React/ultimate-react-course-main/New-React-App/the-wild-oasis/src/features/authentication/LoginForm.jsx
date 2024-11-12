d3c2bd8f41a7cf8ced8ba372ca6f2391abeb8427
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-11-12 22:26:22 +0500

    Authentication + Login + Logout

--- React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -4,7 +4,6 @@ import Form from "../../ui/Form";
 import SpinnerMini from "../../ui/SpinnerMini";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
-import { login } from "../../services/apiAuth";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
