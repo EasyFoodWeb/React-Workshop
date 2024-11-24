c0cf0c4e854a5511c2ea7ce0108de3e6bdfc6f8d
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-11-24 11:48:00 +0500

    React Server Components

---
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>About the Wild Oasis</h1>
+
+      <Link href="/cabins">Explore Luxury cabins.</Link>
+    </div>
+  );
+}
--- React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/page.js
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/page.js
@@ -1,3 +1,9 @@
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>Hello Next</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to the paradise.</h1>
+    </div>
+  );
 }
