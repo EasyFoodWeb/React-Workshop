5d31aca68548307a92eb9441627f725022473a5b
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-11-29 19:04:05 +0500

    SSR SSG PPR features

--- React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/_lib/data-service.js
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -16,6 +17,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
@@ -41,6 +43,8 @@ export const getCabins = async function () {
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
 
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
