2e2f8d281825b3f5e3749b02cdb4adbe4b1abbbf
TL\yousaume <umer.yousaf@constellationdealer.com>
2024-11-30 14:59:28 +0500

    React Client Server components interaction

--- React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/cabins/page.js
+++ React/ultimate-react-course-main/New-React-App/the-wild-oasis-website/app/cabins/page.js
@@ -1,6 +1,8 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 // By Hour
 export const revalidate = 3600;
@@ -9,8 +11,8 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
-  // CHANGE
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
 
   return (
     <div>
@@ -25,8 +27,13 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
